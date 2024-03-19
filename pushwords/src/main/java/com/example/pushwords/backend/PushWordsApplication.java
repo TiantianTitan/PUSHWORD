4b58b924d673b2dd0d09bbb19339be3c62683e9c
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2024-03-19 18:23:32 +0100

    initialiser Spring Boot

---
+++ pushwords/src/main/java/com/example/pushwords/backend/PushWordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pushwords.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushWordsApplication.class, args);
+	}
+
+}
---
+++ pushwords/src/test/java/com/example/pushwords/PushwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pushwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
