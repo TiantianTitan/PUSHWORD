4651437fe141a77499a5168a92c1321f1a2d012f
HaotianXUE <Haotian.Xue@etu.sorbonne-universite.fr>
2024-05-24 15:01:41 +0200

    translate bug

--- PUSHWORDS/src/main/java/com/pushwords/service/WordsApiService.java
+++ PUSHWORDS/src/main/java/com/pushwords/service/WordsApiService.java
@@ -5,16 +5,20 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WordsApiService {
     private static final String API_URL = "https://wordsapiv1.p.rapidapi.com/words/";
-    private static final String API_KEY = "4eca314a24msh01ca7c7cc4be4e7p143556jsn5318d093a358"; // 替换为你的 API 密钥
+    private static final String API_KEY = "4eca314a24msh01ca7c7cc4be4e7p143556jsn5318d093a358";
 
-    public List<String> translateWord(String word) throws IOException {
+    public List<Map<String, String>> translateWord(String word) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet request = new HttpGet(API_URL + word + "/definitions");
         request.addHeader("x-rapidapi-key", API_KEY);
@@ -22,36 +26,25 @@ public class WordsApiService {
 
         try (CloseableHttpResponse response = httpClient.execute(request)) {
             int statusCode = response.getStatusLine().getStatusCode();
-            System.out.println("API Response Status Code: " + statusCode);
-
             if (statusCode == 200) {
                 String result = EntityUtils.toString(response.getEntity());
-                System.out.println("API Response: " + result);
                 return extractDefinitions(result);
             } else {
-                String errorResponse = EntityUtils.toString(response.getEntity());
-                System.err.println("API Error Response: " + errorResponse);
                 throw new IOException("Error: " + statusCode);
             }
         }
     }
 
-    private List<String> extractDefinitions(String jsonString) {
-        List<String> definitions = new ArrayList<>();
-        String startMarker = "\"definition\":\"";
-        String endMarker = "\"";
+    private List<Map<String, String>> extractDefinitions(String jsonString) {
+        List<Map<String, String>> definitions = new ArrayList<>();
+        JSONObject json = new JSONObject(jsonString);
+        JSONArray definitionsArray = json.getJSONArray("definitions");
 
-        int startIndex = 0;
-        while ((startIndex = jsonString.indexOf(startMarker, startIndex)) != -1) {
-            startIndex += startMarker.length();
-            int endIndex = jsonString.indexOf(endMarker, startIndex);
-            if (endIndex != -1) {
-                String definition = jsonString.substring(startIndex, endIndex);
-                definitions.add(definition);
-                startIndex = endIndex + endMarker.length();
-            } else {
-                break;
-            }
+        for (int i = 0; i < definitionsArray.length(); i++) {
+            JSONObject definitionObj = definitionsArray.getJSONObject(i);
+            Map<String, String> definition = new HashMap<>();
+            definition.put("definition", definitionObj.getString("definition"));
+            definitions.add(definition);
         }
 
         return definitions;
